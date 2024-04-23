d89a1d17330a0754d4b9caa251b31b30b9e1212e
kyb3r <raqeeb@duos.ai>
2024-04-23 17:08:51 +1000

    Refactor code to remove unused route.ts file and update tsconfig.json

---
+++ src/app/api/screenshot/route.ts
@@ -0,0 +1,61 @@
+import { NextRequest, NextResponse } from "next/server";
+import puppeteerCore from "puppeteer-core";
+import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium";
+
+export const dynamic = "force-dynamic";
+
+async function getBrowser() {
+  if (process.env.VERCEL_ENV === "production") {
+    console.log("Using chromium");
+    const executablePath = await chromium.executablePath();
+
+    console.log(executablePath);
+
+    const browser = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath,
+      headless: chromium.headless,
+    });
+    return browser;
+  } else {
+    const browser = await puppeteer.launch();
+    return browser;
+  }
+}
+
+export async function GET(request: NextRequest) {
+  const url = request.nextUrl.searchParams.get("url");
+  // const secret = request.nextUrl.searchParams.get("secret");
+
+  // if (secret !== process.env.SECRET) {
+  //   return new NextResponse("Unauthorized", {
+  //     status: 403,
+  //   });
+  // }
+
+  if (!url) {
+    return new NextResponse("No url specified", {
+      status: 400,
+    });
+  }
+
+  const browser = await getBrowser();
+
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1400,
+    height: 900,
+  });
+
+  await page.goto(url);
+  const screenshot = await page.screenshot();
+  await browser.close();
+
+  return new NextResponse(screenshot, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+}
